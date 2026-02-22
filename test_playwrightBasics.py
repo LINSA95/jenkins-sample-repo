0418ba30f35915a9a1247606773bdcc987ca39fc
LINSA95 <71515441+LINSA95@users.noreply.github.com>
2026-02-22 17:20:31 +0530

    Launch browser in headless mode
    Change browser launch to headless mode for tests.

--- test_playwrightBasics.py
+++ test_playwrightBasics.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Page, Playwright, expect
 
 
 def test_playwrightBasics(playwright):
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://rahulshettyacademy.com")
